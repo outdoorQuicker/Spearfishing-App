873c796eda070fa8cd064634979038eb1bcea15e
Michael Ortiz <ortiz.d.michael@gmail.com>
2015-03-06 17:47:41 -0800

    first commit. Added main java classes that will check weather data and then rate it.

---
+++ src/main/java/com/curvejumper/spearconditioncheck/ConditionCheck.java
@@ -0,0 +1,22 @@
+package com.curvejumper.spearconditioncheck;
+
+import com.curvejumper.weatherdata.WeatherRankings;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ *
+ * @author michael
+ */
+class ConditionCheck {
+    //from java scheduler library. used to schedule the 
+    //task of getting weather data and rating how good the conditions are for spearfising
+    private final ScheduledExecutorService scheduler =
+     Executors.newScheduledThreadPool(1);
+    
+    //the main function for refreshing weather rankings
+    public static void refreshConditions(){
+        //list of weather data for diffrent regions
+        WeatherRankings rankings = new WeatherRankings();
+    }
+}
