9fdf4a32fc8b9b519f245449df1e4fa326904e90
Michael Ortiz <ortiz.d.michael@gmail.com>
2015-03-10 21:48:32 -0700

    adding more material for connecting to web sites

--- src/main/java/com/curvejumper/weatherdata/WeatherWebSites.java
+++ src/main/java/com/curvejumper/weatherdata/WeatherWebSites.java
@@ -5,6 +5,8 @@
  */
 package com.curvejumper.weatherdata;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -17,11 +19,21 @@ class WeatherWebSites {
      * This will get the weather website data 
      * it'll store specific weather objects that can be called whenever
      * only for quick access
+     * 
      */
     //get date to know what current time is
-    //you will then want to see what it is closest to
-        //wind site has specific times to get
+    //@see http://www.tutorialspoint.com/java/java_date_time.htm
     Date date = new Date();
+    SimpleDateFormat currentHour = new SimpleDateFormat("hha");
+    //to use do currentHour.format(date)
+    
+    
+    //here's another site
+    //http://spearboard.com/showthread.php?s=5ba8829bc913a7350826824400eeb32a&t=78881
+    
+    
+    //you will then want to see what it is closest to
+    //wind site has specific times to get
     private String windSiteName = "http://www.windfinder.com/forecast/malibu";
     
     private Document windWebSite = Jsoup.connect(windSiteName).get();
